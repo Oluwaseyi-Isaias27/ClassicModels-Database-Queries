a02d2b5956e5b3eb6564e90e2ef66326d8045fcd
Oluwaseyi-Isaias27 <94055998+Oluwaseyi-Isaias27@users.noreply.github.com>
2023-05-27 23:04:49 +0100

    Update query01.sql

--- challenges/singleEntity/query01.sql
+++ challenges/singleEntity/query01.sql
@@ -1,1 +1,6 @@
+-- Question : Prepare a list of offices sorted by country,state,city
+-- backticks is used to reference fieldnames in MySQL
 
+SELECT `territory` as 'Office Location'
+  FROM offices
+  ORDER BY `country`, `state`, `city`;
